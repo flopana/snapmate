20a8c1fb0220c2c0049b0697ac223564417f339d
Florian Erbs <florian@f-erbs.de>
2025-02-08 12:37:17 +0100

    parse timeshift output

--- src/snaphots/snaphot.go
+++ src/snaphots/snaphot.go
@@ -1,10 +1,12 @@
 package snaphots
 
 import (
-	"fmt"
+	"bufio"
+	"errors"
 	"os/exec"
 	"snapmate/config"
 	"snapmate/logger"
+	"strings"
 )
 
 func CreateSnapshot(pacmanArgs string, ppid int, conf config.Conf) error {
@@ -17,14 +19,41 @@ func CreateSnapshot(pacmanArgs string, ppid int, conf config.Conf) error {
 }
 
 func timeshiftCreateSnapshot(pacmanArgs string) error {
+	l := logger.NewLogger()
 	out, err := exec.Command("timeshift", "--create", "--comments", pacmanArgs).Output()
 	if err != nil {
-		l := logger.NewLogger()
 		l.Error("Could not create snapshot")
 		l.Error(string(out))
 
 		return err
 	}
-	fmt.Println(string(out))
+	l.Info("Snapshot created")
+	snapshotName, err := parseTimeshiftOutput(string(out))
+	if err != nil {
+		l.Error("Could not parse timeshift output")
+		l.Error(string(out))
+
+		return err
+	}
+	l.Info("Created snapshot: ", snapshotName)
+
 	return nil
+}
+
+/**
+ * This function is used to parse the output of the timeshift command.
+ * It looks for the line that contains the snapshot name and returns it.
+ */
+func parseTimeshiftOutput(out string) (string, error) {
+	// Tagged snapshot '2025-02-08_12-22-58': ondemand
+	scanner := bufio.NewScanner(strings.NewReader(out))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "Tagged snapshot '") {
+			snapshot := strings.Split(line, "'")[1]
+			return snapshot, nil
+		}
+	}
+
+	return "", errors.New("could not parse timeshift output")
 }
